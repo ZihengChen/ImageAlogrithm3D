2771aede0864877c1d7bd26fac6f4f1f64383f8f
Ziheng Chen <zihengchen@dhcp-10-105-163-98.wireless.northwestern.private>
2018-02-06 12:15:45 -0600

    add ImageAlgo in K dimension

--- scripts/HGCal_ImageAlgo3D_kernel.py
+++ scripts/HGCal_ImageAlgo3D_kernel.py
@@ -3,110 +3,240 @@ import pycuda.driver as cuda
 import pycuda.autoinit
 from pycuda.compiler import SourceModule
 mod = SourceModule("""
-    __global__ void rho_cuda( float *d_rho,
-                              int nrech, float KERNAL_R, float KERNAL_Z, float KERNAL_EXPC,
-                              float *d_x, float *d_y,float *d_z, float *d_e ){
-    int i = blockDim.x*blockIdx.x + threadIdx.x;
-    if(i<nrech){
-        float xi = d_x[i];
-        float yi = d_y[i];
-        float zi = d_z[i];
+
+    __global__ void rho_cuda(float *d_rho,
+                         int nrech, float KERNAL_R, float KERNAL_Z, float KERNAL_EXPC,
+                         float *d_x, float *d_y,float *d_z, float *d_e )
+    {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        if(i<nrech){
+            float xi = d_x[i];
+            float yi = d_y[i];
+            float zi = d_z[i];
     
-        float rhoi = 0.0;
-        for (int j=0;j<nrech; j++){
-            float dr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi));
-            float dz = abs(d_z[j]-zi);
+            float rhoi = 0.0;
+            for (int j=0;j<nrech; j++){
+                float dr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi));
+                float dz = abs(d_z[j]-zi);
             
-            
-            if ( dz<=KERNAL_Z && dr<KERNAL_R ){ 
-                // on some device e.g. Intel Iris Pro, function exp() is not defined
-                // use Tylor expansion for exp() instead
-                // rhoi = rhoi + d_e[j] * exp(- dr/1.0);
-                float d   = KERNAL_EXPC*dr;
-                float exp = 1/(1+d+d*d/2+d*d*d/6+d*d*d*d/24);
-                rhoi = rhoi + d_e[j] * exp;
+                if ( dz<=KERNAL_Z && dr<KERNAL_R ){ 
+                    rhoi = rhoi + d_e[j] * exp(- KERNAL_EXPC*dr);
                 }
             }
-        d_rho[i] = rhoi;
+            d_rho[i] = rhoi;
         }
     }
     
     __global__ void rhoranknh_cuda(int *d_rhorank, int *d_nh, float *d_nhd, 
                                    int nrech, float *d_x, float *d_y,float *d_z,
-                                   float *d_rho ){
-    int i = blockDim.x*blockIdx.x + threadIdx.x;
-    if( i<nrech ){
-        float xi = d_x[i];
-        float yi = d_y[i];
-        float zi = d_z[i];
-        float rhoi = d_rho[i];
+                                   float *d_rho )
+    {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        if( i<nrech ){
+            float xi = d_x[i];
+            float yi = d_y[i];
+            float zi = d_z[i];
+            float rhoi = d_rho[i];
         
-        int rhoranki = 0;
-        int nhi = i;
-        float nhdi = 200.0; // MAXDISTANCE = 200
+            int rhoranki = 0;
+            int nhi = i;
+            float nhdi = 200.0; // MAXDISTANCE = 200
         
-        for (int j=0; j<nrech; j++){
-            if(d_rho[j]>rhoi) rhoranki++;
-            if(d_rho[j]==rhoi){
-               if (j<i) rhoranki++;
-               }
+            for (int j=0; j<nrech; j++){
+                if(d_rho[j]>rhoi) rhoranki++;
+                if(d_rho[j]==rhoi){
+                   if (j<i) rhoranki++;
+                }
                
-            float drr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi)+ (d_z[j]-zi)*(d_z[j]-zi));
-            if ( (drr<nhdi) and (d_rho[j]>rhoi)){ //if nearer AND higher rho
+                float drr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi)+ (d_z[j]-zi)*(d_z[j]-zi));
+                if ( (drr<nhdi) and (d_rho[j]>rhoi)){ //if nearer AND higher rho
                     nhdi = drr;
                     nhi = j;
-                    }
+                }
             }
             
-        d_rhorank[i] = rhoranki;
-        d_nh[i] = nhi;
-        d_nhd[i]= nhdi;
+            d_rhorank[i] = rhoranki;
+            d_nh[i] = nhi;
+            d_nhd[i]= nhdi;
         }
     }
     
     
-    
-    
     // split function for rhorank and nh
-    __global__ void rhorank_cuda(int *d_rhorank, int nrech, float *d_rho ){
-    int i = blockDim.x*blockIdx.x + threadIdx.x;
-    if( i<nrech ){
-        float rhoi = d_rho[i];
+    __global__ void rhorank_cuda(int *d_rhorank, int nrech, float *d_rho )
+    {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        if( i<nrech ){
+            float rhoi = d_rho[i];
         
-        int rhoranki = 0;
-        for (int j=0; j<nrech; j++){
-           if(d_rho[j]>rhoi) rhoranki++;
-           if(d_rho[j]==rhoi){
-               if (j<i) rhoranki++;
-               }
-           }
-        d_rhorank[i] = rhoranki;
+            int rhoranki = 0;
+            for (int j=0; j<nrech; j++){
+                if(d_rho[j]>rhoi) rhoranki++;
+                if(d_rho[j]==rhoi){
+                    if (j<i) rhoranki++;
+                }
+            }
+            d_rhorank[i] = rhoranki;
         }
     }
     
     __global__ void nh_cuda(int *d_nh, float *d_nhd,
                             int nrech, float *d_x, float *d_y,float *d_z,
-                            float *d_rho ){
-    int i = blockDim.x*blockIdx.x + threadIdx.x;
-    if( i<nrech ){
-        float xi = d_x[i];
-        float yi = d_y[i];
-        float zi = d_z[i];
-        float rhoi = d_rho[i];
+                            float *d_rho ) 
+    {
+
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        if( i<nrech ){
+            float xi = d_x[i];
+            float yi = d_y[i];
+            float zi = d_z[i];
+            float rhoi = d_rho[i];
         
-        int nhi = i;
-        float nhdi = 200.0; // MAXDISTANCE = 200
-        for (int j=0; j<nrech; j++){
-            float drr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi)+ (d_z[j]-zi)*(d_z[j]-zi));
-            if ( (drr<nhdi) and (d_rho[j]>rhoi)){ //if nearer
+            int nhi = i;
+            float nhdi = 200.0; // MAXDISTANCE = 200
+            for (int j=0; j<nrech; j++){
+                float drr = sqrt((d_x[j]-xi)*(d_x[j]-xi) + (d_y[j]-yi)*(d_y[j]-yi)+ (d_z[j]-zi)*(d_z[j]-zi));
+                if ( (drr<nhdi) and (d_rho[j]>rhoi)){ //if nearer
                     nhdi = drr;
                     nhi = j;
-                    }
+                }
             }
-        d_nh[i] = nhi;
-        d_nhd[i]= nhdi;
+            d_nh[i] = nhi;
+            d_nhd[i]= nhdi;
         }
     }
+
+
+    
+
+
+
+    __global__ void rho_cuda_smem( float *d_rho,
+                              int nrech, float KERNAL_R, float KERNAL_Z, float KERNAL_EXPC,
+                              float *d_x, float *d_y,float *d_z, float *d_e )
+    {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        const int PARTWIDTH = 1024;
+        __shared__ float x_shared[PARTWIDTH];
+        __shared__ float y_shared[PARTWIDTH];
+        __shared__ float z_shared[PARTWIDTH];
+        __shared__ float e_shared[PARTWIDTH];
+        if(i<nrech){
+
+            float xi = d_x[i];
+            float yi = d_y[i];
+            float zi = d_z[i];
+
+            float rhoi = 0.0;
+        
+            int npart = int (nrech/PARTWIDTH) ;
+            if (nrech%PARTWIDTH>0) npart++;
+
+            for(int ipart=0;ipart<npart;ipart++){
+
+                if (ipart*PARTWIDTH + threadIdx.x < nrech ){
+                    x_shared[threadIdx.x] = d_x[ipart*PARTWIDTH + threadIdx.x];
+                    y_shared[threadIdx.x] = d_y[ipart*PARTWIDTH + threadIdx.x];
+                    z_shared[threadIdx.x] = d_z[ipart*PARTWIDTH + threadIdx.x];
+                    e_shared[threadIdx.x] = d_e[ipart*PARTWIDTH + threadIdx.x];
+                }
+                else{
+                    x_shared[threadIdx.x] = 0;
+                    y_shared[threadIdx.x] = 0;
+                    z_shared[threadIdx.x] = 0;
+                    e_shared[threadIdx.x] = 0;     
+                }
+
+                __syncthreads();
+
+                for (int j=0;(j<PARTWIDTH) && (ipart * PARTWIDTH + j < nrech); j++){
+
+                    float dr = sqrt((x_shared[j]-xi)*(x_shared[j]-xi) + 
+                                    (y_shared[j]-yi)*(y_shared[j]-yi));
+
+                    float dz = abs(z_shared[j]-zi);
+
+                    if ( dz<=KERNAL_Z && dr<KERNAL_R ){ 
+                        rhoi += e_shared[j] * exp(- KERNAL_EXPC*dr);
+                    }
+                }
+                __syncthreads(); 
+            }
+        
+            d_rho[i] = rhoi;
+        }
+    }
+
+__global__ void rhoranknh_cuda_smem(int *d_rhorank, int *d_nh, float *d_nhd, 
+                                   int nrech, float *d_x, float *d_y,float *d_z,
+                                   float *d_rho )
+    {
+        int i = blockDim.x*blockIdx.x + threadIdx.x;
+        const int PARTWIDTH = 1024;
+        __shared__ float x_shared[PARTWIDTH];
+        __shared__ float y_shared[PARTWIDTH];
+        __shared__ float z_shared[PARTWIDTH];
+        __shared__ float rho_shared[PARTWIDTH];
+
+        if( i<nrech ){
+            float xi = d_x[i];
+            float yi = d_y[i];
+            float zi = d_z[i];
+            float rhoi = d_rho[i];
+        
+            int rhoranki = 0;
+            int nhi = i;
+            float nhdi = 200.0; // MAXDISTANCE = 200
+
+
+            int npart = int (nrech/PARTWIDTH) ;
+            if (nrech%PARTWIDTH>0) npart++;
+
+            for(int ipart=0;ipart<npart;ipart++){
+
+                if (ipart*PARTWIDTH + threadIdx.x < nrech ){
+                    x_shared[threadIdx.x] = d_x[ipart*PARTWIDTH + threadIdx.x];
+                    y_shared[threadIdx.x] = d_y[ipart*PARTWIDTH + threadIdx.x];
+                    z_shared[threadIdx.x] = d_z[ipart*PARTWIDTH + threadIdx.x];
+
+                    rho_shared[threadIdx.x] = d_rho[ipart*PARTWIDTH + threadIdx.x];
+                }
+                else{
+                    x_shared[threadIdx.x] = 0;
+                    y_shared[threadIdx.x] = 0;
+                    z_shared[threadIdx.x] = 0;
+
+                    rho_shared[threadIdx.x] = 0;     
+                }
+                __syncthreads();
+
+        
+                for (int j=0;(j<PARTWIDTH) && (ipart * PARTWIDTH + j< nrech); j++){
+                    int j_global = ipart * PARTWIDTH + j ;
+
+                    if(rho_shared[j]>rhoi) rhoranki++;
+                    if(rho_shared[j]==rhoi){
+                        if (j_global < i) rhoranki++;
+                    }
+               
+                    float drr = sqrt((x_shared[j]-xi)*(x_shared[j]-xi) + 
+                                     (y_shared[j]-yi)*(y_shared[j]-yi) + 
+                                     (z_shared[j]-zi)*(z_shared[j]-zi));
+                    // nearer AND higher rho
+                    if ( (drr<nhdi) and (d_rho[j]>rhoi)){ 
+                        nhdi = drr;
+                        nhi = j_global;
+                    }
+                }
+                __syncthreads(); 
+            }
+            
+            d_rhorank[i] = rhoranki;
+            d_nh[i] = nhi;
+            d_nhd[i]= nhdi;
+        }
+    }
+    
     
     
 """)
@@ -114,3 +244,91 @@ rho_cuda       = mod.get_function("rho_cuda")
 rhoranknh_cuda = mod.get_function("rhoranknh_cuda")
 rhorank_cuda   = mod.get_function("rhorank_cuda")
 nh_cuda        = mod.get_function("nh_cuda")
+
+rho_cuda_smem        = mod.get_function("rho_cuda_smem")
+rhoranknh_cuda_smem  = mod.get_function("rhoranknh_cuda_smem")
+
+
+
+
+'''     
+__global__ void rho_cuda_smem2(float *d_rho,
+                              int nrech, float KERNAL_R, float KERNAL_Z, float KERNAL_EXPC,
+                              float *d_x, float *d_y,float *d_z, float *d_e ){
+
+        const int PARTWIDTH = 1024;
+        const int NHITS = 8;
+
+        int idx_starting = NHITS * (blockDim.x*blockIdx.x + threadIdx.x);
+        
+        __shared__ float x_shared[PARTWIDTH];
+        __shared__ float y_shared[PARTWIDTH];
+        __shared__ float z_shared[PARTWIDTH];
+        __shared__ float e_shared[PARTWIDTH];
+
+        if(idx_starting < nrech){
+            float xi[NHITS];
+            float yi[NHITS];
+            float zi[NHITS];
+            float rhoi[NHITS];
+
+            for( int i = 0; (i<NHITS) ; ++i ){
+                xi[i] = 0;
+                yi[i] = 0;
+                zi[i] = 0;
+                rhoi[i] = 0;
+            } 
+
+            for( int i = 0; (i<NHITS) && (idx_starting+i < nrech); ++i ){
+                xi[i] = d_x[idx_starting+i];
+                yi[i] = d_y[idx_starting+i];
+                zi[i] = d_z[idx_starting+i];
+            }
+
+            
+            
+
+            int npart = int (nrech/PARTWIDTH) ;
+            if (nrech%PARTWIDTH>0) npart++;
+
+            for(int ipart=0;ipart<npart;ipart++){
+                // 1. copy partial data
+                if (ipart*PARTWIDTH + threadIdx.x < nrech ){
+                    x_shared[threadIdx.x] = d_x[ipart*PARTWIDTH + threadIdx.x];
+                    y_shared[threadIdx.x] = d_y[ipart*PARTWIDTH + threadIdx.x];
+                    z_shared[threadIdx.x] = d_z[ipart*PARTWIDTH + threadIdx.x];
+                    e_shared[threadIdx.x] = d_e[ipart*PARTWIDTH + threadIdx.x];
+                }
+                else{
+                    x_shared[threadIdx.x] = 0;
+                    y_shared[threadIdx.x] = 0;
+                    z_shared[threadIdx.x] = 0;
+                    e_shared[threadIdx.x] = 0;     
+                }   
+                __syncthreads();
+
+                // 2. compute partial result
+                for( int i = 0; (i<NHITS) && (idx_starting+i < nrech); ++i ){                    
+                    
+                    for (int j=0; (j<PARTWIDTH) && (ipart * PARTWIDTH + j < nrech); j++){
+
+                        float dr = sqrt(( x_shared[j] - xi[i] ) * ( x_shared[j] - xi[i] ) + 
+                                        ( y_shared[j] - yi[i] ) * ( y_shared[j] - yi[i] )
+                                        );
+
+                        float dz = abs( z_shared[j] - zi[i] );
+
+                        if ( dz<=KERNAL_Z && dr<KERNAL_R ){ 
+                            rhoi[i] += e_shared[j] * exp(- KERNAL_EXPC*dr);
+                        }
+                    }
+                }
+                __syncthreads(); 
+            }
+
+            for( int i = 0; (i<NHITS) && (idx_starting+i < nrech); ++i ){
+                d_rho[ idx_starting+i ] = rhoi[i];
+            }
+        }
+    }
+    '''
---
+++ scripts/ImageAlgoKD.py
@@ -0,0 +1,180 @@
+from pylab import *
+import pandas as pd
+from ImageAlgoKD_kernel_cuda import *
+
+class ImageAlgoKD():
+    def __init__(self,
+                 MAXDISTANCE        = 1.5,
+                 KERNAL_R           = 0.7,
+                 KERNAL_R_NORM      = 0.5,
+                 KERNAL_POWER       = 2.0,
+                 DECISION_RHO_KAPPA = 4.0,
+                 DECISION_NHD       = 0.7,
+                 CONTINUITY_NHD     = 0.7
+                ):
+        
+        self.MAXDISTANCE    = MAXDISTANCE
+        self.KERNAL_R       = KERNAL_R
+        self.KERNAL_R_NORM  = KERNAL_R_NORM
+        self.KERNAL_POWER   = KERNAL_POWER
+        self.DECISION_RHO_KAPPA = DECISION_RHO_KAPPA
+        self.DECISION_NHD   = DECISION_NHD
+        self.CONTINUITY_NHD = CONTINUITY_NHD
+    
+    def run_ImageAlgoKD_CUDA(self, Points, wPoints, ReturnDF=False):
+        nPoints,kPoints = Points.shape
+        
+        Points  = Points .astype(np.float32)
+        wPoints = wPoints.astype(np.float32)
+        nPoints = np.int32(nPoints)
+        kPoints = np.int32(kPoints)
+    
+        rho     = np.zeros(nPoints).astype(np.float32)
+        rhorank = np.zeros(nPoints).astype(np.int32)
+        nh      = np.zeros(nPoints).astype(np.int32)
+        nhd     = np.zeros(nPoints).astype(np.float32)
+        
+        d_Points  = cuda.mem_alloc(Points.nbytes)
+        d_wPoints = cuda.mem_alloc(wPoints.nbytes)
+        d_rho     = cuda.mem_alloc(rho.nbytes)
+        d_rhorank = cuda.mem_alloc(rhorank.nbytes)
+        d_nh      = cuda.mem_alloc(nh.nbytes)
+        d_nhd     = cuda.mem_alloc(nhd.nbytes)
+
+        ### Run Cuda ###
+        cuda.memcpy_htod( d_Points , Points )
+        cuda.memcpy_htod( d_wPoints, wPoints )
+
+        rho_cuda(d_rho, d_Points, d_wPoints,
+                 nPoints, kPoints, 
+                 np.float32(self.KERNAL_R), np.float32(self.KERNAL_R_NORM),np.float32(self.KERNAL_POWER),
+                 grid=(int(nPoints/1024)+1,1,1),block=(int(1024),1,1))
+
+        rhoranknh_cuda(d_rhorank, d_nh,d_nhd, d_Points, d_rho,
+                       nPoints, kPoints, 
+                       np.float32(self.MAXDISTANCE),
+                       grid=(int(nPoints/1024)+1,1,1),block=(int(1024),1,1))
+
+
+        cuda.memcpy_dtoh(rho,d_rho)
+        cuda.memcpy_dtoh(rhorank,d_rhorank)
+        cuda.memcpy_dtoh(nh,d_nh)
+        cuda.memcpy_dtoh(nhd,d_nhd)
+        ### Finish Cuda ###
+
+
+        d_Points.free()
+        d_wPoints.free()
+
+        d_rho.free()
+        d_rhorank.free()
+        d_nh.free()
+        d_nhd.free()
+        
+        
+        cluster = -np.ones(nPoints,int)
+        self.DECISION_RHO = rho.max()/self.DECISION_RHO_KAPPA
+
+        # 2.1 convert rhorank to argsortrho 0(N)
+        argsortrho = np.zeros(nPoints,int)
+        argsortrho[rhorank] = np.arange(nPoints)
+
+        # 2.2 find seeds
+        selectseed = (rho>self.DECISION_RHO) & (nhd>self.DECISION_NHD)
+        seedrho = rho[selectseed]
+        temp = seedrho.argsort()[::-1]
+        seedid = np.empty(len(seedrho), int)
+        seedid[temp] = np.arange(len(seedrho))
+        cluster[selectseed] = seedid
+
+        # 2.3 asign clusters to seeds
+        for ith in range(nPoints):
+            i = argsortrho[ith]
+            if  (cluster[i]<0) & (nhd[i]<self.CONTINUITY_NHD):
+                cluster[i] = cluster[nh[i]]
+                
+        
+        if ReturnDF:
+            result = pd.DataFrame()
+            result['rho'] = pd.Series(rho)
+            result['rhorank'] = pd.Series(rhorank)
+            result['nh'] = pd.Series(nh)
+            result['nhd'] = pd.Series(nhd)
+            result['isseed'] = pd.Series(selectseed)
+            result['cluster'] = pd.Series(cluster)
+            return result
+        else:
+            return cluster
+    
+
+    def run_ImageAlgoKD_numpy(self, Points, wPoints, ReturnDF=False):
+        nPoints,kPoints = Points.shape
+
+        # 1. find rho 
+        rho = []
+        for i in range(nPoints):
+            dr = dis(points, points[i])
+            local = (dr<self.KERNAL_R)
+            irho = np.sum( points_weight[local] * np.exp( -dr[local]/self.KERNAL_R_NORM) )
+            rho.append(irho)
+
+        rho = np.array(rho)
+
+
+        # 2. find rhorank
+        argsortrho = rho.argsort()[::-1]
+        rhorank = np.empty(len(rho), int)
+        rhorank[argsortrho] = np.arange(len(rho))
+
+
+
+        # 3. find NearstHiger and distance to NearestHigher
+        nh,nhd = [],[]
+        for i in range(nPoints):
+            irho  = rho[i]
+            irank = rhorank[i]
+            
+            higher = rho>irho
+            # if no points is higher
+            if not (True in higher): 
+                nh. append(i)
+                nhd.append(self.MAXDISTANCE)
+            else:
+                drr  = dis(points[higher], points[i])
+                temp = np.arange(len(rho))[higher]
+                nh. append(temp[np.argmin(drr)])
+                nhd.append(np.min(drr))
+                    
+        nh = np.array(nh)
+        nhd= np.array(nhd)
+        
+        # 4 Assign cluster
+        cluster = -np.ones(nPoints,int)
+        self.DECISION_RHO = rho.max()/self.DECISION_RHO_KAPPA
+
+        # 4.1 make decision and find seeds
+        selectseed = (rho>self.DECISION_RHO) & (nhd>self.DECISION_NHD)
+        seedrho = rho[selectseed]
+        temp = seedrho.argsort()[::-1]
+        seedid = np.empty(len(seedrho), int)
+        seedid[temp] = np.arange(len(seedrho))
+        cluster[selectseed] = seedid
+
+        # 4.2 asign clusters to seeds
+        for ith in range(nPoints):
+            i = argsortrho[ith]
+            if  (cluster[i]<0) & (nhd[i]<self.CONTINUITY_NHD):
+                cluster[i] = cluster[nh[i]]
+        
+        # 5. output
+        if ReturnDF:
+            result = pd.DataFrame()
+            result['rho'] = pd.Series(rho)
+            result['rhorank'] = pd.Series(rhorank)
+            result['nh'] = pd.Series(nh)
+            result['nhd'] = pd.Series(nhd)
+            result['isseed'] = pd.Series(selectseed)
+            result['cluster'] = pd.Series(cluster)
+            return result
+        else:
+            return cluster
